c343a66cb7035148746b2ed9d07ac1f4ae20395d
Chipyoka <mwapechipyoka18@gmail.com>
2025-01-19 01:17:18 +0200

    Updated port number

--- index.js
+++ index.js
@@ -124,6 +124,6 @@ app.get('/api/users/:_id/logs', async (req, res) => {
 
 
 
-const listener = app.listen(process.env.PORT || 8080, () => {
+const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
 })
